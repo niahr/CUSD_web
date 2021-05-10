cb7184c7a3324634360ca67d181b0a8db88a46be
HiteshMantrabuddi <hm564@cornell.edu>
2021-05-10 16:48:29 -0400

    small fixes

--- src/components/weareateam.js
+++ src/components/weareateam.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import pic from '../images/weare.png'
 import * as iconStyle from "./weare.module.css"
+import { Link } from "gatsby"
 
 export default function team({ children }) {
   return <div className={iconStyle.weare}>{children}
@@ -9,6 +10,7 @@ export default function team({ children }) {
     <p className ={iconStyle.para}>Founded in 2009, Cornell University Sustainable Design (CUSD) is a student run, 
     interdisciplinary project team with a bias towards impact. We bring together students from across all seven colleges to work 
     alongside renowned faculty and knowledgeable professionals in our mission to create a network of proactively sustainable communities.</p>
+    <Link to="/about/" className={iconStyle.link}>Learn more about the team</Link>
   </div>
   <div className={iconStyle.image}> <img src ={pic} height = '400px'/> </div>
   </div>
