c3997338df62329100eb619df9ca4b88bda37acc
HiteshMantrabuddi <hm564@cornell.edu>
2021-09-20 15:40:46 -0400

    Nav Bar and Underlines

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import Footer from '../components/Footer'
 import { Link } from "gatsby"
 import Connectwithus from '../components/connectwithus'
 import Team from '../components/weareateam'
+import Nav from '../components/nav'
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <Link to="/about/">about</Link>
-      <Footer />
+      <Nav/>
       <Team/>
+      <Footer />
       <Connectwithus/>
       
     </main>
