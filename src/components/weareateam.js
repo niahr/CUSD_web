c371463afbd23cde0c6538fdee1fd1c7625e89c9
gls245@cornell.edu <gls245@cornell.edu>
2021-10-04 11:08:04 -0400

    fixed formatting

--- src/components/weareateam.js
+++ src/components/weareateam.js
@@ -7,8 +7,7 @@ import Animate from './Animate'
 export default function team({ children }) {
   return <div className={iconStyle.weare}>{children}
   <div className={iconStyle.left}>
-    <h1 className = {iconStyle.header }> We are a Team of </h1>
-    <Animate/>
+    <h1 className = {iconStyle.header }> We are a Team of <Animate/></h1>
     <p className ={iconStyle.para}>Founded in 2009, Cornell University Sustainable Design (CUSD) is a student run, 
     interdisciplinary project team with a bias towards impact. We bring together students from across all seven colleges to work 
     alongside renowned faculty and knowledgeable professionals in our mission to create a network of proactively sustainable communities.</p>
