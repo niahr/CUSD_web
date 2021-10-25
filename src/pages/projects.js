31859f3e7ab676094d19be8fa2b7e22f091e75eb
judysng <judy3096@gmail.com>
2021-10-25 17:20:04 -0400

    add the rest of the projects and icons in two columns

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,10 +3,24 @@ import { Link } from "gatsby"
 import ProjectList from '../projectscomponents/project'
 import Nav from '../components/nav'
 
-// markup
+import susedunepal from "../icons/susedunepal.png";
+import arc from "../icons/arc.png"
+import susmob from "../icons/susmob.png"
+import aqua from "../icons/aqua.png"
 
-const projects = [{id: 1, name: 'Sustainable Education Nepal', subtitle: 'CUSD x United World Schools'},
-                  {id: 2, name: 'ARC', subtitle: 'Alternative Recycling Cornell'}]
+// markup
+const projects = [{id: 1, name: 'Sustainable Education Nepal', subtitle: 'CUSD x United World Schools', pic: susedunepal},
+                  {id: 2, name: 'ARC', subtitle: 'Alternative Recycling Cornell', pic: arc},
+                  {id: 3, name: 'Sustainable Mobility', subtitle: 'Re-design the current public transportation system in Tompkins County, NY', pic: susmob},
+                  {id: 4, name: 'AquaCulture', subtitle: 'Revolutionizing Fishing Industry with Marine Aquaculture', pic: aqua},
+                  {id: 5, name: 'Soil Factory', subtitle: 'Defining the next generation of sustainable design'},
+                  {id: 6, name: 'Solar Panel Reboot', subtitle: 'Defining the next generation of sustainable design'},
+                  {id: 7, name: 'Currents', subtitle: 'Defining the next generation of sustainable design'},
+                  {id: 8, name: 'ICN 2030 Policy', subtitle: 'Ithaca Carbon Neautral 2030 Policy'},
+                  {id: 9, name: 'REAL', subtitle: 'Empowering local farms through the system of Aquaponics'},
+                  {id: 10, name: 'SST', subtitle: 'Smithsonian Sustainability Team'},
+                  {id: 11, name: 'Solarize', subtitle: 'Electrifying Cornell Grounds with sustainable energy sources'},
+                  {id: 12, name: 'ICN 2030 Neutrality Modeling', subtitle: 'ICN 2030 Neutrality Modeling'}]
 
 const ProjectPage = () => {
   return (
--- src/projectscomponents/project.js
+++ src/projectscomponents/project.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import * as iconStyle from "./project.module.css"
 
 export default function projectList({ projects }) {
     const content = projects.map((project) =>
-        <div key={project.id}>
-        <h3>{project.name}</h3>
-        <p>{project.subtitle}</p>
+        <div key={project.id} className={iconStyle.projects}>
+            <img src={project.pic} className={iconStyle.logos}/>
+            <h3 className={iconStyle.name}>{project.name}</h3>
+            <p className={iconStyle.subtitle}>{project.subtitle}</p>
         </div>
     );
     return (
