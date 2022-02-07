54d3981f782745764525a661cc4040e85d25d8c6
sh845@cornell.edu <seokyoung99@gmail.com>
2022-02-07 17:33:23 -0500

    reorganized join

--- src/components/join/content.js
+++ src/components/join/content.js
@@ -10,22 +10,26 @@ const Content = () => {
 
 
       <div className="text-container">
-        <h2> Why Should you join CUSD?</h2>
+        <h2> Why Should You Join CUSD?</h2>
         <h5> You shouldn't xD XD XD XDDDDD XDDDX DX D jk plz do because of these
           well written reasons that are succinct and compelling: text text text
           text text text texttext texttext texttext texttext texttext text text texttext texttext texttext texttext texttext text
-          text texttext texttext texttext texttext texttext text
+          text texttext texttext texttext texttext texttext textwell written reasons that are succinct and compelling: text text text
+          text text text texttext texttext texttext texttext texttext text text texttext texttext texttext texttext texttext text
+          text texttext texttext texttext texttext texttext textwell written reasons that are succinct and compelling: text text text
+          text text text texttext texttext texttext texttext texttext text text texttext texttext texttext texttext texttext text
+          text texttext texttext texttext tex
         </h5>
       </div>
 
-      <div class="buttonContainer">
+      <div class="button-container">
 
         <div>
           <img src={joinimg} alt="joinimg" className="joinimg"></img>
         </div>
 
         <div className="joinButton">
-          <a rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSde1gix0npGShmcuuTwRyhxN20NTyP__oKlIGvDJJBl4rBSLw/viewform?usp=sf_link" target="_blank" className="joinButtonInner"> Join Now!</a>
+          <a rel="noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSde1gix0npGShmcuuTwRyhxN20NTyP__oKlIGvDJJBl4rBSLw/viewform?usp=sf_link" target="_blank" className="joinButtonInner"> Apply Now!</a>
         </div>
       </div>
 
