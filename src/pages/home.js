d0665b967f54c6f4439d50a65d6c3b416bf13d3e
map452 <map452@cornell.edu>
2022-05-13 18:10:24 -0400

    About Page Presentation

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { Lines } from 'react-preloaders';
+import home from '../components/landing/home';
+ 
+function App() {
+  return (
+      <home />
+  );
+}
+
+export default home
